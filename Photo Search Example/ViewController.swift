842918c8fbbc217f275b505ce468918ca4525d89
asr2230 <asr2230@cs.rit.edu>
2017-07-04 15:13:40 -0400

    Made the program show pictures related to search terms.

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UISearchBarDelegate {
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchFlickrBy("dogs")
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Utility methods
+    func searchFlickrBy(_ searchString:String) {
+        let manager = AFHTTPSessionManager()
+        let searchParameters:[String:Any] = ["method": "flickr.photos.search",
+                                             "api_key": "5ea5e7fbbad7c8dd6975c687f4b872e2",
+                                             "format": "json",
+                                             "nojsoncallback": 1,
+                                             "text": searchString,
+                                             "extras": "url_m",
+                                             "per_page": 5]
+        manager.get("https://api.flickr.com/services/rest/",
+                    parameters: searchParameters,
+                    progress: nil,
+                    success: { (operation: URLSessionDataTask, responseObject:Any?) in
+                        if let responseObject = responseObject {
+                            print("Response: " + (responseObject as AnyObject).description)
+                            if let photos = (responseObject as AnyObject)["photos"] as? [String: AnyObject] {
+                                if let photoArray = photos["photo"] as? [[String: AnyObject]] {
+                                    self.scrollView.contentSize = CGSize(width: 320, height: 320 * CGFloat(photoArray.count))
+                                    for (i,photoDictionary) in photoArray.enumerated() {
+                                        if let imageURLString = photoDictionary["url_m"] as? String {
+                                            let imageView = UIImageView(frame: CGRect(x:0, y:320*CGFloat(i), width:320, height:320))
+                                            if let url = URL(string: imageURLString) {
+                                                imageView.setImageWith(url)
+                                                self.scrollView.addSubview(imageView)
+                                            }
+                                            
+                                        }
+                                    }
+                                }
+                            }
+                            
+                        }
+        }) { (operation:URLSessionDataTask?, error:Error) in
+            print("Error: " + error.localizedDescription)
+        }
+        
     }
-
-
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        for subview in self.scrollView.subviews {
+            subview.removeFromSuperview()
+        }
+        searchBar.resignFirstResponder()
+        if let searchText = searchBar.text {
+            searchFlickrBy(searchText)
+        }
+    }
 }
-
